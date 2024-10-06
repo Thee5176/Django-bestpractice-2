ea59c04052eb0c608f69576100c12bdd02a0ecb2
Thee5176 <theerapong5176@gmail.com>
2024-10-06 17:17:23 +0900

    Write model test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+class PostTests(TestCase):
+    """Model test"""
+    @classmethod
+    def setUpTestData(cls):
+        """Store in class instance"""
+        cls.post = Post.objects.create(text="This is a test.")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test.")
