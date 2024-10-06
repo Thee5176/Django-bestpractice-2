c702482a68e0c663f07c6415bc8fe0b4a65f417f
Thee5176 <theerapong5176@gmail.com>
2024-10-06 17:27:36 +0900

    Write testcases

--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Post
 class PostTests(TestCase):
@@ -10,3 +11,22 @@ class PostTests(TestCase):
 
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test.")
+
+class HomePageViewTest(TestCase):
+    def test_url_exist_at_correct_url(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+    
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Message board homepage")
+    
+    
